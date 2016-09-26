31f689c7fda6413df2455c002e1fff1261d3d884
jangolluan <jangolluan@aeoncredit.com.ph>
2016-09-26 18:01:44 +0800

    new update

--- src/main/java/com/acss/controller/MainController.java
+++ src/main/java/com/acss/controller/MainController.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.portlet.ModelAndView;
 
 @Controller
 public class MainController {
 	
+	
 	@RequestMapping(value = {"/index.htm"}, method = {RequestMethod.GET})
 	public ModelAndView doGet(HttpServletRequest request, HttpServletResponse response) {
-		return new ModelAndView("index");
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		mv.addObject("SAMPLE", "SAMPLE");
+		
+		return mv;
 	}
 }
